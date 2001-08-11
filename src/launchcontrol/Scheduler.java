b9c37811cab37eebc049d59eb17cbac8d3b9bcbe
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2001-08-11 09:32:47 +0000

    *** empty log message ***
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@73 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/launchcontrol/Scheduler.java
+++ src/launchcontrol/Scheduler.java
@@ -10,12 +10,14 @@ public class Scheduler
 		types.put("msg", new MessageAction());
 	}
 
-	private Timer timer = null;
-	private URL events = null;
-	private Properties conf = null;
+	private URL events;
+	private Timer timer;
+
 	private ScheduleListener listener = null;
+	private int millidelta;
+
 	private int startTime, endTime; // in milliseconds
-	private Date started = null;
+	private Date started;
 
 	/**
 	 * Create a new Scheduler from an event list. The event list is not
@@ -29,32 +31,97 @@ public class Scheduler
 		this.events = events;
 	}
 
-	public synchronized void startCountdown(ScheduleListener l, int millidelta)
-		throws Exception
+	/**
+	 * Requests that the given ScheduleListener be called once every delta
+	 * milliseconds during a countdown. Only takes effect on the next call
+	 * to startCountdown() or abortCountdown(). Only the most recently
+	 * added listener will recieve schedule events.
+	 */
+	public void addScheduleListener(ScheduleListener l, int delta)
+	{
+		if(l == null)
+			return; // no action when given a null listener
+		listener = l;
+		millidelta = delta;
+	}
+
+	/**
+	 * Reads the events file and begin executing it. Events are executed in
+	 * a separate thread. No exception is thrown nor any action taken if the
+	 * countdown has already started.
+	 */
+	public void startCountdown() throws IOException
 	{
 		if(timer != null)
-			throw new IllegalStateException("timer already running");
-		listener = l;
+			return; // can't re-start a running countdown.
+
 		if(listener != null)
 			listener.started();
 
-		started = new Date(new Date().getTime() + 1000);
+		// read events file for general settings
+		Properties conf = new Properties();
+		conf.load(events.openStream());
+		startTime = endTime = 0;
 
-		conf = new Properties();
-		conf.load(events.openStream()); // read once for general settings
-		startTime = (int)(Float.parseFloat(conf.getProperty("startTime", "0")) * 1000);
-		endTime = (int)(Float.parseFloat(conf.getProperty("endTime", "0")) * 1000);
+		try {
+			String start = conf.getProperty("startTime");
+			if(start != null)
+				startTime = (int)(Float.parseFloat(start) * 1000);
+		} catch(NumberFormatException e) {
+			// ignore
+		}
+		try {
+			String end = conf.getProperty("endTime");
+			if(end != null)
+				endTime = (int)(Float.parseFloat(end) * 1000);
+		} catch(NumberFormatException e) {
+			// ignore
+		}
+
+		setTimer(/*aborting*/ false);
+	}
+
+	/**
+	 * Moves the countdown to time 0 if that time hasn't yet been reached.
+	 * No exeception is thrown nor any action taken if the countdown has
+	 * passed time 0, nor if the countdown is currently stopped.
+	 */
+	public void abortCountdown() throws IOException
+	{
+		if(timer == null)
+			return; // can't cancel a countdown that isn't running.
+		if((new Date().getTime() - started.getTime() + startTime) > 0)
+			return; // it's too late, you can't abort now. muhuhahaha.
+
+		if(listener != null)
+			listener.aborted();
+
+		timer.cancel();
+		timer = null;
+
+		setTimer(/*aborting*/ true);
+	}
+
+	/**
+	 * Initializes the Timer instance from the events file. Used by both
+	 * startCountdown() and abortCountdown() with the aborting parameter
+	 * set to false and true, respectively. When aborting, events set to
+	 * execute before time 0 are ignored, and startTime is forced to 0.
+	 */
+	private void setTimer(boolean aborting) throws IOException
+	{
+		// don't start until everything has probably been set up
+		// tweak the number in the following line for an appropriate delay
+		started = new Date(new Date().getTime() + 250);
+
+		if(aborting)
+			startTime = 0;
 
 		timer = new Timer();
-		if(listener != null && millidelta > 0)
-			timer.scheduleAtFixedRate(new TimerTask() {
-				public void run()
-				{
-					listener.time(new Date().getTime() - started.getTime() + startTime);
-				}
-			}, started, millidelta);
-
-		BufferedReader cs = new BufferedReader(new InputStreamReader(events.openStream())); // read again for timing
+		// read events file for timing
+		BufferedReader cs = new BufferedReader(
+			new InputStreamReader(events.openStream())
+		);
 		String line;
 		while((line = cs.readLine()) != null)
 		{
@@ -69,10 +136,19 @@ public class Scheduler
 				continue; // wasn't a number; try the next line
 			}
 
+			if(aborting && time <= 0)
+				continue; // when aborting, run only positive-scheduled events
+
+			// sanity check start and end times
+			if(time < startTime) // assertion: if aborting, this is always false.
+				startTime = time;
+			if((time + 1) > endTime)
+				endTime = time + 1;
+
 			String value = line.substring(colon + 1).trim();
+			String action, cmd;
+			// split on first comma
 			int comma = value.indexOf(',');
-			String action;
-			String cmd;
 			if(comma != -1)
 			{
 				action = value.substring(0, comma).trim();
@@ -83,50 +159,45 @@ public class Scheduler
 				action = value;
 				cmd = "";
 			}
+
+			// schedule this event
 			timer.schedule(new ScheduledTask(action, cmd),
 				new Date(started.getTime() + time - startTime));
-
-			// sanity check start and end times
-			if(time < startTime)
-				startTime = time;
-			if(time > endTime)
-				endTime = time;
 		}
 
+		// schedule the repeated clock updates as requested for the listener
+		if(listener != null && millidelta > 0)
+			timer.scheduleAtFixedRate(new TimerTask() {
+				public void run()
+				{
+					listener.time(new Date().getTime() - started.getTime() + startTime);
+				}
+			}, started, millidelta);
+
+		// schedule cleanup at endTime
 		timer.schedule(new TimerTask() {
 			public void run()
 			{
-				timer.cancel();
-				timer = null;
-				conf = null;
 				if(listener != null)
 					listener.ended();
-				listener = null;
+
+				timer.cancel();
+				timer = null;
 			}
 		}, new Date(started.getTime() + endTime - startTime));
 	}
 
-	public synchronized void abortCountdown() throws Exception
-	{
-		if(timer == null)
-			throw new IllegalStateException("timer not running");
-		timer.cancel();
-		timer = null;
-		conf = null;
-		if(listener != null)
-			listener.aborted();
-		listener = null;
-	}
-
 	private class ScheduledTask extends TimerTask
 	{
-		private SchedulableAction action = null;
-		private String cmd = null;
+		final SchedulableAction action;
+		final String cmd;
 
-		public ScheduledTask(String action, String cmd)
+		public ScheduledTask(String actstr, String c)
 		{
-			this.action = (SchedulableAction)types.get(action);
-			this.cmd = cmd;
+			action = (SchedulableAction)types.get(actstr);
+			if(action == null)
+				throw new IllegalArgumentException("unknown action " + actstr);
+			cmd = c;
 		}
 
 		public void run()
