a2868a026c31ae0c12bef919d1f95ca47de223a9
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2002-09-15 08:01:11 +0000

    Moved into cansocket package and added getData methods.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@189 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/cansocket/CanMessage.java
+++ src/cansocket/CanMessage.java
@@ -1,3 +1,5 @@
+package cansocket;
+
 public class CanMessage
 {
   protected short id;
@@ -28,4 +30,23 @@ public class CanMessage
     return body;
   }
 
+  public byte getData8(int i)
+  {
+    return body[i];
+  }
+
+  public short getData16(int i)
+  {
+    i <<= 1;
+    return (short) (body[i] << 8 | body[i + 1]);
+  }
+
+  public int getData32(int i)
+  {
+    i <<= 2;
+    return body[i] << 24 |
+           body[i + 1] << 16 |
+           body[i + 2] << 8 |
+           body[i + 3];
+  }
 }
