9048cf61a148b02e6db7eece8a196236d2634373
dcassard <dcassard@6115d4fc-9e01-0410-8836-dad5f2c52705>
2002-09-18 06:54:04 +0000

    add message decoding and display
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@219 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/TimeObserver.java
+++ src/rocketview/TimeObserver.java
@@ -13,8 +13,34 @@ class TimeObserver extends JLabel implements Observer
 		CanMessage msg = (CanMessage) arg;
 		switch(msg.getId())
 		{
-		default:
-			/* do nothing */
+                case CanBusIDs.GPSTime: {
+                    Byte year = new Byte(msg.getData8(0));
+                    Byte month = new Byte(msg.getData8(1));
+                    Byte day = new Byte(msg.getData8(2));
+                    Byte hour = new Byte(msg.getData8(3));
+                    Byte minute = new Byte(msg.getData8(4));
+                    Byte second = new Byte(msg.getData8(5));
+                    
+                    StringBuffer buf = new StringBuffer();
+                    Integer bigYear = new Integer( year.intValue() + 2000 );
+                    buf.append( bigYear.toString() );
+                    buf.append( "/" );
+                    buf.append( month.toString() );
+                    buf.append( "/" );
+                    buf.append( day.toString() );
+                    
+                    buf.append( " " );
+                    buf.append( hour.toString() );
+                    buf.append( ":" );
+                    buf.append( minute.toString() );
+                    buf.append( ":" );
+                    buf.append( second.toString() );
+                    
+                    setText( buf.toString() );
+                    
+               }
+                default:
+			/* message not for me; do nothing */
 		}
 	}
 }
