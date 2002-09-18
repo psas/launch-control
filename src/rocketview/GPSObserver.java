6b5192d57964638a5ea696ca9687508a17c0f04c
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2002-09-18 06:59:09 +0000

    Filled in GPSObserver with GPS status sources.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@220 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/GPSObserver.java
+++ src/rocketview/GPSObserver.java
@@ -11,10 +11,24 @@ class GPSObserver extends JLabel implements Observer
 	public void update(Observable o, Object arg)
 	{
 		CanMessage msg = (CanMessage) arg;
-		switch(msg.getId())
-		{
-		default:
-			/* do nothing */
-		}
+		if(msg.getId() != CanBusIDs.GPSStatus)
+			return;
+		StringBuffer buf = new StringBuffer();
+		if(msg.getData8(1) != 0 || msg.getData8(2) != 0)
+			buf.append("not ");
+		buf.append("locked, ");
+		buf.append(msg.getData8(0)).append(" sats, ");
+		appendBinary(buf, msg.getData8(1)).append(' ');
+		appendBinary(buf, msg.getData8(2));
+		setText(buf.toString());
+	}
+
+	protected StringBuffer appendBinary(StringBuffer buf, byte v)
+	{
+		String s = Integer.toBinaryString(v);
+		for(int i = 8 - s.length(); i > 0; --i)
+			buf.append('0');
+		buf.append(s);
+		return buf;
 	}
 }
