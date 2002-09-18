6abcadf43d0d4732869ae6d8dc48bb78dc58f8b2
dcassard <dcassard@6115d4fc-9e01-0410-8836-dad5f2c52705>
2002-09-18 07:23:10 +0000

    simplify a bit
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@222 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/TimeObserver.java
+++ src/rocketview/TimeObserver.java
@@ -13,32 +13,31 @@ class TimeObserver extends JLabel implements Observer
 		CanMessage msg = (CanMessage) arg;
 		switch(msg.getId())
 		{
-                case CanBusIDs.GPSTime: {
-                    Byte year = new Byte(msg.getData8(0));
-                    Byte month = new Byte(msg.getData8(1));
-                    Byte day = new Byte(msg.getData8(2));
-                    Byte hour = new Byte(msg.getData8(3));
-                    Byte minute = new Byte(msg.getData8(4));
-                    Byte second = new Byte(msg.getData8(5));
+                    case CanBusIDs.GPSTime:
+                        byte year = msg.getData8(0);
+                        byte month = msg.getData8(1);
+                        byte day = msg.getData8(2);
+                        byte hour = msg.getData8(3);
+                        byte minute = msg.getData8(4);
+                        byte second = msg.getData8(5);
+                        
+                        StringBuffer buf = new StringBuffer();
+                        int bigYear =  year + 2000;
+                        buf.append( bigYear );
+                        buf.append( "/" );
+                        buf.append( month );
+                        buf.append( "/" );
+                        buf.append( day );
+                        
+                        buf.append( " " );
+                        buf.append( hour );
+                        buf.append( ":" );
+                        buf.append( minute );
+                        buf.append( ":" );
+                        buf.append( second );
+                        
+                        setText( buf.toString() );
                     
-                    StringBuffer buf = new StringBuffer();
-                    Integer bigYear = new Integer( year.intValue() + 2000 );
-                    buf.append( bigYear.toString() );
-                    buf.append( "/" );
-                    buf.append( month.toString() );
-                    buf.append( "/" );
-                    buf.append( day.toString() );
-                    
-                    buf.append( " " );
-                    buf.append( hour.toString() );
-                    buf.append( ":" );
-                    buf.append( minute.toString() );
-                    buf.append( ":" );
-                    buf.append( second.toString() );
-                    
-                    setText( buf.toString() );
-                    
-               }
                 default:
 			/* message not for me; do nothing */
 		}
