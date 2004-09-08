b6ed73b188ada5b17e066db2d5d84dc97fc94042
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2004-09-08 00:09:04 +0000

    Revised to use widgets.RocketState for state label.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@1038 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/launchcontrol/RocketPanel.java
+++ src/launchcontrol/RocketPanel.java
@@ -1,6 +1,7 @@
 package launchcontrol;
 
 import cansocket.*;
+import widgets.*;
 
 import java.io.*;
 import java.awt.*;
@@ -9,36 +10,9 @@ import javax.swing.*;
 
 public class RocketPanel extends JPanel
 {
-	private JLabel statusLabel = new JLabel();
+	protected final RocketState statusLabel = new RocketState();
 
-	protected int state;
 	protected CanSocket sock;
-
-	protected static final int StatusID = Config.getInt("rocket.status", 8);
-	protected static final int RocketReadyState = Config.getInt("rocket.stReady", 7);
-	
-	protected static final int StatReqID = Config.getInt("rocket.request", 0);
-	
-	protected static final String[] stateStrings = {
-		"TURNED ON",
-		"POWER UP",
-		"INITIALIZE",
-		"IDLE",
-		"PREFLIGHT CHECK",
-		"READY",
-		"ARMED",
-		"ROCKET READY",
-		"LAUNCH ABORT",
-		"BOOST",
-		"COAST",
-		"DEPLOY DROGUE",
-		"DESCEND DROGUE",
-		"DEPLOY MAIN",
-		"DESCEND MAIN",
-		"RECOVERY WAIT",
-		"RECOVERY SLEEP",
-		"POWER DOWN"
-	};
 
 	public RocketPanel(CanSocket socket)
 	{
@@ -49,28 +23,7 @@ public class RocketPanel extends JPanel
 		setLayout(new BorderLayout());
 		add(statusLabel, BorderLayout.CENTER);
 
-		setState(-1);
 		new ReaderThread().start();
-	}
-
-	protected void setState(int state)
-	{
-		this.state = state;
-		if(state < 0)
-			statusLabel.setText("Unknown state");
-		else if(state >= stateStrings.length)
-			statusLabel.setText("Unknown state (" + Integer.toString(state) + ")");
-		else
-			statusLabel.setText(stateStrings[state]);
-	}
-
-	protected void changeState() throws IOException
-	{
-		if(state == CanBusIDs.IdleState)
-		{
-			byte[] preflight = { CanBusIDs.PreflightCheckState };
-			sock.write(new CanMessage(0, CanBusIDs.FC_REQUEST_STATE >> 5, 0, 1, preflight));
-		}
 	}
 
 	private class ReaderThread extends Thread
@@ -80,17 +33,7 @@ public class RocketPanel extends JPanel
 			try {
 				NetMessage m;
 				while ((m = sock.read()) != null)
-				{
-					if ( !(m instanceof CanMessage) )
-						continue;
-
-					CanMessage c = (CanMessage) m;
-					if(c.getId11() != CanBusIDs.FC_REPORT_STATE >> 5)
-						continue;
-
-					setState(c.getData8(0));
-					changeState();
-				}
+					statusLabel.update(m);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
