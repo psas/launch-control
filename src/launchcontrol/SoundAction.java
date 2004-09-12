a60afa0f33c998e7140cee8f3d33b9ae0c8eea84
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2004-09-12 18:45:20 +0000

    Get sounds from the classpath so it works in a JAR.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@1106 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/launchcontrol/SoundAction.java
+++ src/launchcontrol/SoundAction.java
@@ -14,7 +14,7 @@ public class SoundAction implements SchedulableAction
 	public static void playSound(String file) throws Exception
 	{
 // 		Manager.createRealizedPlayer(new File(file).toURL()).start();
-		AudioInputStream stream = AudioSystem.getAudioInputStream(new File(file));
+		AudioInputStream stream = AudioSystem.getAudioInputStream(ClassLoader.getSystemResource(file));
 		DataLine.Info info = new DataLine.Info(Clip.class, stream.getFormat());
 		Clip line = (Clip)AudioSystem.getLine(info);
 		line.open(stream);
