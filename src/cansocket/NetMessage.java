d209bcb71fdb73afbe7dd5c855c0e3bf7584f0a6
shark <shark@6115d4fc-9e01-0410-8836-dad5f2c52705>
2004-09-12 00:01:58 +0000

    made names match with fifo_msg.h, re-synced values, and added message types.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@1080 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/cansocket/NetMessage.java
+++ src/cansocket/NetMessage.java
@@ -15,13 +15,17 @@ public abstract class NetMessage
 
 	public static final short FC_PROT_VER             = 2;
 
-	public static final short NET_MSG_TYPE_CAN        = 0;
+	public static final short FMT_CAN        = 0;
 	// 1 is timer message (FC only)
-	public static final short NET_MSG_TYPE_GPS_ORIGIN = 2;
-	public static final short NET_MSG_TYPE_PRESSURE   = 3;
+	public static final short FMT_GPS_ORIGIN = 1; // message from ground only
+	public static final short FMT_PRESSURE_BASE   = 2; // message from ground only
+
+	public static final short FMT_IMU_ACCEL = 3;
+	public static final short FMT_IMU_GYRO = 4;
+	public static final short FMT_PRESSURE_DATA = 5;
 	
 	/* abstract messages */
-	public abstract byte[] toByteArray();
+	public byte[] toByteArray() { return new byte[MAX_MSG_SIZE]; }
 	public abstract void putMessage(DataOutputStream dos);
 
 	/* factory */
@@ -38,10 +42,18 @@ public abstract class NetMessage
 		
 		switch (type)
 		{
-			case NET_MSG_TYPE_CAN:
+			case FMT_CAN:
 				return new CanMessage(s);
+			case FMT_IMU_ACCEL:
+				return new ImuAccelMessage(s);
+			case FMT_IMU_GYRO:
+				return new ImuGyroMessage(s);
+			case FMT_PRESSURE_DATA:
+				return new PressureDataMessage(s);
 
-			case NET_MSG_TYPE_GPS_ORIGIN:
+			// The following two will only be sent to the rocket, 
+			// so they can be implemented when we need to do that.
+			case FMT_GPS_ORIGIN:
 			{
 				double lat  = s.readDouble();
 				double lng  = s.readDouble();
@@ -49,11 +61,11 @@ public abstract class NetMessage
 				// FIXME: return new GPSMessage(s);
 			}
 			break;
-			case NET_MSG_TYPE_PRESSURE:
+			case FMT_PRESSURE_BASE:
 			{
 				double press = s.readDouble();
 				double temp  = s.readDouble();
-				// FIXME: return new PressureMessage(s);
+				// FIXME: return new PressureBaseMessage(s);
 			}
 			break;
 		}
