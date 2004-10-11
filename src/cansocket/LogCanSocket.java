d13d044600ae54b990aaaa3a0d3eab2b815d352a
shark <shark@6115d4fc-9e01-0410-8836-dad5f2c52705>
2004-10-11 09:47:08 +0000

    Added timestamping for Rocketview's logfile.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@1230 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/cansocket/LogCanSocket.java
+++ src/cansocket/LogCanSocket.java
@@ -1,16 +1,22 @@
 package cansocket;
 
 import java.io.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class LogCanSocket implements CanSocket
 {
 	CanSocket base;
 	Writer log;
+	SimpleDateFormat myformat;
 
 	public LogCanSocket(CanSocket base, String logfile) throws IOException
 	{
 		this.base = base;
 		log = new FileWriter(logfile, /* append */ true);
+		/* timestamp log messages as e.g. "Fri, 15 Oct 2004 13:59:59:025 PDT": */
+		myformat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss:SSS z ");
+
 	}
 
 	public NetMessage read() throws IOException
@@ -40,7 +46,8 @@ public class LogCanSocket implements CanSocket
 		}
 		buf.append('\n');
 */
-		log.write(msg.toString());
+		Date cur_time = new Date();
+		log.write( myformat.format(cur_time) + msg.toString() );
 		log.write("\n");
 		log.flush();
 
