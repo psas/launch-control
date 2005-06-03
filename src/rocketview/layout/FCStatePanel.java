b95e3115816bd078777c774aa45501dfd10cd38f
twelch <twelch@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-03 07:57:35 +0000

    Setup driver
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2196 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/layout/FCStatePanel.java
+++ src/rocketview/layout/FCStatePanel.java
@@ -1,6 +1,33 @@
 package rocketview.layout;
 
-public class FCStatePanel
+import java.awt.FlowLayout;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import widgets.*;
+
+/**
+ * Panel containing the flight computer state "lights"
+ */
+public class FCStatePanel extends JPanel
 {
-    
+	
+	public FCStatePanel () {
+		
+		this.setBorder(new TitledBorder("FC State"));
+		this.setLayout(new FlowLayout( FlowLayout.LEFT, 5, 0 /* no vert spacing */));
+		
+		/** TODO: Group RocketState/StateGrid lights into separate
+		 *  sections/panels IMU, GPS, APS, etc.*/
+		this.add(new RocketState());
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println( "FCStatePanel test driver" );
+		JFrame frame = new JFrame("FCstatePanel test driver");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new FCStatePanel());
+		frame.pack();
+	    frame.setVisible(true);
+	}
 }
