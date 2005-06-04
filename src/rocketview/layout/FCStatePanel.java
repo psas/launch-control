e46454d564e730b36280a5a77e5ea64799d8d25a
twelch <twelch@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-04 07:02:56 +0000

    added canListener
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2202 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/layout/FCStatePanel.java
+++ src/rocketview/layout/FCStatePanel.java
@@ -1,29 +1,42 @@
 package rocketview.layout;
 
 import java.awt.FlowLayout;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+
+import cansocket.*;
 import widgets.*;
 
 /**
- * Panel containing the flight computer state "lights"
+ * Panel containing rocketState widget
  */
 public class FCStatePanel extends JPanel
 {
-	
-	public FCStatePanel () {
-		
+
+	/** constructor */
+	public FCStatePanel () 
+	{
 		this.setBorder(new TitledBorder("FC State"));
-		this.setLayout(new FlowLayout( FlowLayout.LEFT, 5, 0 /* no vert spacing */));
-		
-		/** TODO: Group RocketState/StateGrid lights into separate
-		 *  sections/panels IMU, GPS, APS, etc.*/
+		this.setLayout(new FlowLayout( FlowLayout.LEFT, 5, 0 /* no vert spacing */));		
 		this.add(new RocketState());
 	}
 	
-	public static void main(String[] args)
+	/**
+	 * FCStatePanel test driver
+	 * @throws IOException if canMessage logging fails
+	 */
+	public static void main(String[] args) throws IOException
 	{
-		System.out.println( "FCStatePanel test driver" );
+		//canMessage log
+        StringBuffer tmpDir = new StringBuffer(System.getProperty("java.io.tmpdir"));
+        String log = tmpDir.append(File.separatorChar).append("fcs.log").toString();
+        
+        CanSocket socket = new LogCanSocket(new UDPCanSocket(), log); 
+        CanListener listener = new CanListener(socket);
+        
 		JFrame frame = new JFrame("FCstatePanel test driver");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(new FCStatePanel());
