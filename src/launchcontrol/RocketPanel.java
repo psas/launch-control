fffa8ce40463680003b821eec8948d744fc3bfcd
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-07 21:53:24 +0000

    RocketState and CanDispatch can do everything this class did, so transform it into a little wrapper.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2246 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/launchcontrol/RocketPanel.java
+++ src/launchcontrol/RocketPanel.java
@@ -3,39 +3,13 @@ package launchcontrol;
 import cansocket.*;
 import widgets.*;
 
-import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-public class RocketPanel extends JPanel
+public class RocketPanel extends RocketState
 {
-	protected final RocketState statusLabel = new RocketState();
-	protected CanSocket sock;
-
 	public RocketPanel(CanSocket socket, LaunchControl parent)
 	{
-		sock = socket;
-		statusLabel.addLinkStateListener(parent);
-
-		setLayout(new FlowLayout());
-		// add status for rocket ready
-		add(statusLabel);
-		
-		new ReaderThread().start();
-	}
-
-	private class ReaderThread extends Thread
-	{
-		public void run()
-		{
-			try {
-				CanMessage m;
-				while ((m = sock.read()) != null)
-					statusLabel.update(m);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		addLinkStateListener(parent);
+		CanDispatch dispatch = new CanDispatch(socket);
+		dispatch.add(this);
+		new Thread(dispatch).start();
 	}
 }
