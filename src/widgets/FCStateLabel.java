90ba2feb2e5be4b2cf4e7d85362eff1fa8930a2c
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-07 19:22:25 +0000

    Refactor the flight computer state label out of RocketState.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2229 6115d4fc-9e01-0410-8836-dad5f2c52705

---
+++ src/widgets/FCStateLabel.java
@@ -0,0 +1,114 @@
+package widgets;
+
+import cansocket.*;
+
+import java.text.*;
+import java.util.*;
+
+public class FCStateLabel extends StateLabel implements CanObserver
+{
+	protected static final String[] stateStrings = {
+		"EvaluatePowerup",
+		"Initialize",
+		"Idle",
+		"PreflightCheck",
+		"Ready",
+		"Arming",
+		"Armed",
+		"RocketReady",
+		"LaunchAbort",
+		"Boost",
+		"Coast",
+		"DeployDrogue",
+		"DescendDrogue",
+		"DeployMain",
+		"DescendMain",
+		"RecoveryWait",
+		"RecoverySleep",
+		"PowerDown",
+		"LawnDart",
+	};
+
+	protected static final DateFormat df = new SimpleDateFormat("HH:mm:ss.SSS: ");
+	protected static final long delay = 1200; /* link timeout delay (millisecs) */
+
+	protected final Timer timer = new Timer(true /*daemon*/);
+
+	protected String signal = "-/-";
+	protected String state = "-";
+
+	protected LinkStateChecker task;
+	protected LinkStateListener listener;
+
+	protected class LinkStateChecker extends TimerTask
+	{
+		public void run()
+		{
+			// If the task is activated, then it has been too long since
+			// we recieved a message
+			setKnown(false);
+		}
+	}
+
+	public FCStateLabel()
+	{
+		super("");
+		updateText();
+	}
+
+	public void addLinkStateListener(LinkStateListener listener)
+	{
+		this.listener = listener;
+	}
+
+	public void message(CanMessage msg)
+	{
+		switch(msg.getId())
+		{
+			case CanBusIDs.FC_REPORT_STATE:
+				setState(msg.getData8(0) & 0xff);
+				break;
+			case CanBusIDs.FC_REPORT_LINK_QUALITY:
+				setQuality(msg.getData16(0), msg.getData16(1));
+				break;
+		}
+	}
+
+	protected void setKnown(boolean known)
+	{
+		super.setKnown(known);
+		if(listener != null)
+			listener.linkStateChanged(known);
+	}
+
+	protected void setState(int state)
+	{
+		String date = df.format(new Date());
+		if(state >= stateStrings.length)
+			this.state = date + "unknown (" + state + ")";
+		else
+			this.state = date + stateStrings[state];
+		updateText();
+		setState(true);
+		setKnown(true);
+
+		// set a task to run after delay milliseconds, which will happen
+		// unless we've recieved a message (and thus entered this function)
+		// before that time
+		if (task != null)
+			task.cancel();
+		task = new LinkStateChecker();
+		timer.schedule(task, delay, 1000);
+	}
+
+	protected void setQuality(short signal, short noise)
+	{
+		this.signal = "" + -signal + "/" + -noise + "dBm";
+		updateText();
+	}
+
+	protected void updateText()
+	{
+		setText("s/n: " + signal + ", " + state);
+	}
+}
--- src/widgets/RocketState.java
+++ src/widgets/RocketState.java
@@ -2,85 +2,26 @@ package widgets;
 
 import cansocket.*;
 
-import java.text.*;
 import java.util.*;
 import javax.swing.*;
-import java.awt.GridLayout;
 
 public class RocketState extends JPanel implements Observer
 {
-
-	protected static final String[] stateStrings = {
-		"EvaluatePowerup",
-		"Initialize",
-		"Idle",
-		"PreflightCheck",
-		"Ready",
-		"Arming",
-		"Armed",
-		"RocketReady",
-		"LaunchAbort",
-		"Boost",
-		"Coast",
-		"DeployDrogue",
-		"DescendDrogue",
-		"DeployMain",
-		"DescendMain",
-		"RecoveryWait",
-		"RecoverySleep",
-		"PowerDown",
-		"LawnDart",
-	};
-
-	protected static final DateFormat df = new SimpleDateFormat("HH:mm:ss.SSS: ");
-
-	protected ImageIcon greenled = new ImageIcon(ClassLoader.getSystemResource("widgets/greenled.png"));
-	protected ImageIcon redled = new ImageIcon(ClassLoader.getSystemResource("widgets/redled.png"));
-
-	protected int state = -1;
-	protected int detail = -1;
-	protected int signal = 0;		// units: dbm
-	protected int noise = 0;
-	protected String dateStr = "";
-
-	protected static final long delay = 1200; /* link timeout delay (millisecs) */
-	protected final java.util.Timer timer = new java.util.Timer(true /*daemon*/);
-	protected LinkStateChecker task;
-	protected LinkStateListener listener;
-
-	protected JLabel stateLabel;
-	protected CanDispatch dispatch = new CanDispatch();
-
-	protected class LinkStateChecker extends TimerTask
-	{
-		public void run()
-		{
-			// If the task is activated, then it has been too long since
-			// we recieved a message
-			stateLabel.setIcon(redled);
-			if(listener != null)
-				listener.linkStateChanged(false);
-		}
-	}
-
+	protected final FCStateLabel stateLabel = new FCStateLabel();
+	protected final CanDispatch dispatch = new CanDispatch();
 
 	public RocketState()
 	{
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-		stateLabel = new JLabel();
-		stateLabel.setAlignmentX(0.0f);
-		updateText(); // get label ready for display 
+		dispatch.add(stateLabel);
 		add(stateLabel);
-
 		add(new JSeparator());
-
 		add(new StateGrid(dispatch));
 	}
 
 	public void addLinkStateListener(LinkStateListener listener)
 	{
-		this.listener = listener;
+		stateLabel.addLinkStateListener(listener);
 	}
 
 	public void update(Observable o, Object arg)
@@ -94,67 +35,5 @@ public class RocketState extends JPanel implements Observer
 	public void update(CanMessage msg)
 	{
 		dispatch.update(msg);
-
-		switch(msg.getId())
-		{
-			case CanBusIDs.FC_REPORT_STATE:
-				setState(msg.getData8(0) & 0xff);
-				break;
-			case CanBusIDs.FC_REPORT_LINK_QUALITY:
-				setQuality(msg.getData16(0), msg.getData16(1));
-				break;
-		}
-	}
-
-	protected void setState(int state)
-	{
-		if(this.state != state)
-			detail = -1;
-		this.dateStr = df.format(new Date());
-		this.state = state;
-		updateText();
-
-		// set a task to run after delay milliseconds, which will happen
-		// unless we've recieved a message (and thus entered this function)
-		// before that time
-		stateLabel.setIcon(greenled);
-		if(listener != null)
-			listener.linkStateChanged(true);
-		if (task != null)
-			task.cancel();
-		task = new LinkStateChecker(); // will set "led" to red
-		timer.schedule(task, delay, 1000);
-	}
-
-	protected void setQuality(short signal, short noise)
-	{
-		this.signal = -signal;
-		this.noise = -noise;
-		updateText();
-	}
-
-	protected void updateText()
-	{
-		StringBuffer b = new StringBuffer("s/n: ");
-		b.append(Integer.toString(signal)).append("/");
-		b.append(Integer.toString(noise)).append("dBm, ");
-		if(state < 0)
-		{
-			b.append("unknown FC state");
-			stateLabel.setText(b.toString());
-			stateLabel.setIcon(redled);
-			if(listener != null)
-				listener.linkStateChanged(false);
-			return;
-		}
-		b.append(dateStr);
-		if(state >= stateStrings.length)
-			b.append("unknown (").append(state).append(")");
-		else
-			b.append(stateStrings[state]);
-		if(detail >= 0)
-			b.append(" [").append(Integer.toBinaryString(detail)).append("]");
-		stateLabel.setText(b.toString());
-
 	}
 }
