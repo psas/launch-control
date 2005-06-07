a1d41b8c0d5d35385f940a631f665e674108384e
twelch <twelch@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-07 20:16:20 +0000

    set stripcharts preferred size so that default layout looks good
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2234 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/IMUObserver.java
+++ src/rocketview/IMUObserver.java
@@ -19,6 +19,8 @@ import javax.swing.border.*;
  */
 class IMUObserver extends JPanel implements Observer
 {
+	protected static final Dimension MINIMUM_CHART_DIM = new Dimension(300,50); 
+	
 	// first subscript in arrays is one of these
 	protected final int IMU_ACCEL = 0;
 	protected final int IMU_GYRO = 1;
@@ -59,6 +61,7 @@ class IMUObserver extends JPanel implements Observer
 	protected void createChart(int type, int num)
 	{
 		StripChart chart = new StripChart();
+		chart.setPreferredSize(MINIMUM_CHART_DIM);
 		data[type][num] = chart;
 
 		// data.setYRange(vLow[type][num], vHigh[type][num]);
