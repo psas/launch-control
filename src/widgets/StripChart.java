440bc4ef152e42a127bfd7be0e20c45c8f11ec6f
keithp <keithp@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-08 05:39:36 +0000

    keithp and cworth: Add profile-launchview ant target.
    Change StripChart to draw with graphics.drawLine rather than graphics2d.draw(path)
    which seems to fix its performance problems.
    
    
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2261 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/widgets/StripChart.java
+++ src/widgets/StripChart.java
@@ -76,9 +76,11 @@ public class StripChart extends JComponent
 		return ((Float) it.previous()).floatValue();
 	}
 
-	private float constructPath(GeneralPath path)
+	protected void paintComponent(Graphics g)
 	{
-		float old;
+		if (ts.size() == 0)
+			return;
+
 		synchronized(ts)
 		{
 			ListIterator ti = ts.listIterator(ts.size());
@@ -86,34 +88,29 @@ public class StripChart extends JComponent
 
 			float t;
 			float now = previousFloat(ti);
-			old = now - getTimeScale();
+			float old = now - getTimeScale();
+			float x1, y1, x2, y2;
 
-			path.moveTo(now, previousFloat(yi));
-			while(ti.hasPrevious() && (t = previousFloat(ti)) > old)
-				path.lineTo(t, previousFloat(yi));
+			Dimension size = getSize();
+
+			float sx =  size.width / getTimeScale();
+			float sy = -size.height / (maxY - minY);
+			float tx = -old * sx;
+			float ty = -maxY * sy;
+
+			x1 = now;
+			y1 = previousFloat(yi);
+			while(ti.hasPrevious() && (t = previousFloat(ti)) > old) 
+			{
+				x2 = t;
+				y2 = previousFloat(yi);
+				g.drawLine ((int) (x1 * sx + tx + 0.5),
+					    (int) (y1 * sy + ty + 0.5),
+					    (int) (x2 * sx + tx + 0.5),
+					    (int) (y2 * sy + ty + 0.5));
+				x1 = x2;
+				y1 = y2;
+			}
 		}
-
-		return old;
-	}
-
-	protected void paintComponent(Graphics g_base)
-	{
-		if(ts.size() == 0)
-			return;
-
-		Graphics2D g = (Graphics2D) g_base;
-		AffineTransform oldTransform = g.getTransform();
-		GeneralPath path = new GeneralPath();
-		float old = constructPath(path);
-
-		Dimension size = getSize();
-		/* Note: scaling Y by negative factor because Java2D's
-		 * Y axis is positive down, and we want positive up. */
-		g.scale(size.width / getTimeScale(), -size.height / (maxY - minY));
-
-		g.translate(-old, -maxY);
-
-		g.draw(path);
-		g.setTransform(oldTransform);
 	}
 }
