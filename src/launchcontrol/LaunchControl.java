ffd8b736b69cea986e80faef30638a2718f678c3
twelch <twelch@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-09 17:17:14 +0000

    reorganized layout and layout code so it works better with rocketview, functionality is the same
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2285 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/launchcontrol/LaunchControl.java
+++ src/launchcontrol/LaunchControl.java
@@ -15,17 +15,13 @@ public class LaunchControl extends JPanel
 	implements ScheduleListener, LinkStateListener, ActionListener
 {
 	// Class constants
-	protected final static String startMsg = "Start Countdown";
 	protected final static String stopMsg = "ABORT";
+	protected final static String startMsg = "Start Countdown";
 	protected final static String stoppedMsg = "Countdown stopped";
 
 	// Member variables, presumably used by multiple methods
 	protected final DecimalFormat fmt = new DecimalFormat("T+0.0;T-0.0");
-	protected JButton countdownButton = new JButton();
 	protected java.util.Timer powerSequence = null; // power on or off fc
-	
-	protected JLabel clock = new JLabel();
-	protected JLabel statusLabel = new JLabel();
 	protected String startSound = Config.getString("startSound");
 	protected String abortSound = Config.getString("abortSound");
 
@@ -33,8 +29,22 @@ public class LaunchControl extends JPanel
 	protected TCPCanSocket towerSocket;
 
 	protected final Scheduler sched = new Scheduler();
-
-
+	
+	// Launch control panels and components, tabbed to show hierarchy
+	protected JPanel topLCPanel;
+		protected JLabel statusLabel;
+		protected JLabel clock;
+		protected ShorePower shorePowerState;
+	protected JPanel bottomLCPanel;
+		protected JPanel countdownPanel;
+			protected JButton preFlightCheckButton;
+			protected JButton armButton;
+			protected JButton countdownButton;
+			protected JButton abortButton;
+		protected JPanel fcPowerPanel;
+			protected JButton fcPowerOnButton;
+			protected JButton powerDownButton;
+			protected JButton fcPowerOffButton;
 
 	/** Create LaunchControl GUI, open connections, and start scheduler */
 	public LaunchControl(CanDispatch dispatch) throws IOException
@@ -45,101 +55,89 @@ public class LaunchControl extends JPanel
 		// easily and uniformly.
 		
 		rocketSocket = new UDPCanSocket(Config.getString("rocket.host"), Config.getInt("rocket.port", UDPCanSocket.PORT_SEND));
-		dispatch.setSocket(rocketSocket);
+			dispatch.setSocket(rocketSocket);
 		
-		JButton preFlightCheckButton = new JButton("Preflight Check");
-		JButton armButton = new JButton("Arm Rocket");
-		JButton abortButton = new JButton(stopMsg);
-		JButton fcPowerOnButton = new JButton("FC On");
-		JButton powerDownButton = new JButton("Powerdown FC");
-		JButton fcPowerOffButton = new JButton("FC Off");
+		towerSocket = new TCPCanSocket(Config.getString("tower.host"), 
+			Config.getInt("tower.port", TCPCanSocket.DEFAULT_PORT));
+			Scheduler.addSchedulableAction("tower", new SocketAction(towerSocket));
 	
-
-		Box layout_box;
-		Component x_spacer = Box.createRigidArea(new Dimension(5,0));
-		Component y_spacer = Box.createRigidArea(new Dimension(0,10));
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-		layout_box = new Box(BoxLayout.X_AXIS);
-		layout_box.add(x_spacer);
-		layout_box.add(clock);
-		layout_box.add(x_spacer);
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		// setup top panel
+		topLCPanel = new JPanel();
+		topLCPanel.setLayout(new BoxLayout(topLCPanel, BoxLayout.X_AXIS));
+		
+		// setup top components
+		clock = new JLabel();
+		statusLabel = new JLabel();
 		statusLabel.setBorder(BorderFactory.createLoweredBevelBorder());
-		layout_box.add(statusLabel);
-		layout_box.add(x_spacer);
-		add(layout_box);
-		add(y_spacer);
-
-
-		// Sequence buttons
-		layout_box = new Box(BoxLayout.X_AXIS);
-		JPanel countdown_pane = new JPanel();
-		countdown_pane.setLayout(new GridLayout(0,1));
-		countdown_pane.add(preFlightCheckButton);
+		
+		// add top components
+		topLCPanel.add(clock);
+		topLCPanel.add(statusLabel);
+		topLCPanel.add(new ShorePower(towerSocket, "Shore"));
+		this.add(topLCPanel);
+		
+		// setup bottom panel
+		bottomLCPanel = new JPanel();
+		bottomLCPanel.setLayout(new BoxLayout(bottomLCPanel, BoxLayout.X_AXIS));
+		this.add(bottomLCPanel);
+		
+		// setup countdown panel
+		countdownPanel = new JPanel();
+		countdownPanel.setLayout(new GridLayout(0, 1));
+		
+		// setup countdown components
+		preFlightCheckButton = new JButton("Preflight Check");
 		preFlightCheckButton.setActionCommand("preflight");
 		preFlightCheckButton.addActionListener(this);
-
-		countdown_pane.add(armButton);
-		armButton.setActionCommand("arm");
-		armButton.addActionListener(this);
-
-		countdown_pane.add(countdownButton);
-		countdownButton.addActionListener(this);
-
-		Dimension countdown_size = countdown_pane.getPreferredSize();
-		abortButton.setPreferredSize(countdown_size);
-		abortButton.setBackground(Color.red);
-		abortButton.setMaximumSize(countdown_size);
+        armButton = new JButton("Arm Rocket");
+        armButton.setActionCommand("arm");
+        armButton.addActionListener(this);
+        countdownButton = new JButton();
+        countdownButton.addActionListener(this);
 		
-		layout_box.add(x_spacer);
-		layout_box.add(countdown_pane);
-		layout_box.add(x_spacer);
-		layout_box.add(abortButton);
-		layout_box.add(x_spacer);
+		// add countdown components
+		countdownPanel.add(preFlightCheckButton);
+		countdownPanel.add(armButton);
+		countdownPanel.add(countdownButton);
+		bottomLCPanel.add(countdownPanel);
+		
+		// setup/add abort button
+		abortButton = new JButton("ABORT");
 		abortButton.setActionCommand("abort");
 		abortButton.addActionListener(this);
-
-		add(layout_box); 
-		add(y_spacer);
-
-	
-
-		// Power buttons 
-		layout_box = new Box(BoxLayout.X_AXIS);
-		layout_box.add(x_spacer);
-		JPanel fcpower_pane = new JPanel();
-		fcpower_pane.setLayout(new GridLayout(0, 1));
-
-		fcpower_pane.add(fcPowerOnButton);
+		abortButton.setPreferredSize(countdownPanel.getPreferredSize());
+		abortButton.setBackground(Color.red);
+		abortButton.setMaximumSize(countdownPanel.getPreferredSize());
+		bottomLCPanel.add(abortButton);
+		
+		// setup power panel
+		fcPowerPanel = new JPanel();
+		fcPowerPanel.setLayout(new GridLayout(0, 1));
+		
+		// setup power components
+		fcPowerOnButton = new JButton("FC On");
 		fcPowerOnButton.setActionCommand("fc_on");
 		fcPowerOnButton.addActionListener(this);
-
-		fcpower_pane.add(powerDownButton);
+		powerDownButton = new JButton("Powerdown FC");
 		powerDownButton.setActionCommand("powerdown");
 		powerDownButton.addActionListener(this);
-
-		fcpower_pane.add(fcPowerOffButton);
+		fcPowerOffButton = new JButton("FC Off");
 		fcPowerOffButton.setActionCommand("fc_off");
 		fcPowerOffButton.addActionListener(this);
-
-		towerSocket = new TCPCanSocket(Config.getString("tower.host"), 
-				Config.getInt("tower.port", TCPCanSocket.DEFAULT_PORT));
-		Scheduler.addSchedulableAction("tower", new SocketAction(towerSocket));
-
-		ShorePower shorePowerState = new ShorePower(towerSocket, "Shore");
-		layout_box.add(fcpower_pane);
-		layout_box.add(Box.createHorizontalGlue());
-		layout_box.add(Box.createRigidArea(new Dimension(30, 0)));
-		layout_box.add(shorePowerState);
-		layout_box.add(Box.createRigidArea(new Dimension(30, 0)));
-
-		add(layout_box);
-		add(y_spacer);
-
-
+		
+		// add power components
+		fcPowerPanel.add(fcPowerOnButton);
+		fcPowerPanel.add(powerDownButton);
+		fcPowerPanel.add(fcPowerOffButton);
+		bottomLCPanel.add(fcPowerPanel);
+		
+		// add rocketSocket to scheduler
 		Scheduler.addSchedulableAction("rocket", new SocketAction(rocketSocket));
 		ended(); // reset the button and label
 		sched.addScheduleListener(this, 100);
+		
 	}
 
 
