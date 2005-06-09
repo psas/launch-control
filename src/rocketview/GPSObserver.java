3affc74e3ec7fddf4ca91ee0fd2f2a4e9f7a4695
twelch <twelch@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-09 05:23:16 +0000

    Added Lat/Lon observing, from GPSPositionObserver, consolidating into one class
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2278 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/GPSObserver.java
+++ src/rocketview/GPSObserver.java
@@ -1,5 +1,7 @@
 package rocketview;
 
+import java.text.DecimalFormat;
+
 import cansocket.*;
 import widgets.*;
 
@@ -7,24 +9,31 @@ import javax.swing.*;
 
 class GPSObserver extends JPanel implements CanObserver
 {
+	// static fields
+	protected static DecimalFormat minFmt = new DecimalFormat("00.000");
+	
+	// fields
 	int visible = 0;
 	int used = 0;
+	StringBuffer latString;
+	StringBuffer lonString;
 
-	protected final GPSPositionObserver pos = new GPSPositionObserver();
-	protected final JLabel alt = new JLabel("altitude: unknown");
+	protected final JLabel lat = new JLabel("Lat: - N");
+	protected final JLabel lon = new JLabel("Lon: - W");
+	protected final JLabel alt = new JLabel("Alt: -");
 	protected final TimeObserver time = new TimeObserver();
-	protected final JLabel sats = new JLabel("sats: 0/0");
+	protected final JLabel sats = new JLabel("Sats: -/-");
 	protected final LockStateLabel lock = new LockStateLabel();
 
 	public GPSObserver(CanDispatch dispatch)
 	{
 		dispatch.add(this);
-		dispatch.add(pos);
 		dispatch.add(time);
 		dispatch.add(lock);
 
 		setLayout(new GridBoxLayout());
-		add(pos);
+		add(lat);
+		add(lon);
 		add(alt);
 		add(time);
 		add(sats);
@@ -36,8 +45,17 @@ class GPSObserver extends JPanel implements CanObserver
 		switch(msg.getId())
 		{
 			case CanBusIDs.FC_GPS_HEIGHT:
-				alt.setText("altitude: " + (msg.getData32(0) / (float)100.0) + 'm');
+				alt.setText("Alt: " + (msg.getData32(0) / (float)100.0) + 'm');
 				return;
+			case CanBusIDs.FC_GPS_LATLON:
+				latString = new StringBuffer();
+				dir(latString, msg.getData32(0), 'N', 'S');
+				lat.setText("Lat:  " + latString.toString());
+				
+				lonString = new StringBuffer();
+				dir(lonString, msg.getData32(1), 'E', 'W');
+				lon.setText("Lon: " + lonString.toString());
+				break;
 			case CanBusIDs.FC_GPS_SATS_VIS:
 				visible = msg.getData8(0);
 				break;
@@ -48,7 +66,25 @@ class GPSObserver extends JPanel implements CanObserver
 				return;
 		}
 
-		sats.setText("sats: " + used + '/' + visible);
+		sats.setText("Sats: " + used + '/' + visible);
+	}
+	
+	/**
+	 * dir constructs formatted lat/lon string
+	 * @param b - StringBuffer to put formatted position data
+	 * @param mag - 32 bit fixed point lat or lon data
+	 * @param pos - N or E
+	 * @param neg - S or W
+	 */
+	protected static void dir(StringBuffer b, int mag, char pos, char neg)
+	{
+		char sgn = mag < 0 ? neg : pos;
+		float rad = Math.abs(mag) / (float)100000000.0;
+		double deg = Math.toDegrees(rad);
+		double degOnly = Math.floor(deg);
+		double minutes = (deg - degOnly) * 60.0;
+		// unicode degree character
+		b.append(Math.round(degOnly)).append("\u00b0 ").append(minFmt.format(minutes)).append(sgn);
 	}
 
 	private static class LockStateLabel extends StateLabel implements CanObserver
@@ -60,7 +96,7 @@ class GPSObserver extends JPanel implements CanObserver
 
 		private static String makeName(int lockbits)
 		{
-			StringBuffer buf = new StringBuffer("lock: 0x");
+			StringBuffer buf = new StringBuffer("Lock: 0x");
 			String hex = Integer.toHexString(lockbits);
 			for(int i = 8 - hex.length(); i > 0; --i)
 				buf.append('0');
