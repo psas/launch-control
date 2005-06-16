9b8cb829c1a73bd94dcd4edd934397aa6fe1097a
iano <iano@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-16 03:34:58 +0000

    Make it boring.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2294 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/BooleanStateLabel.java
+++ src/rocketview/BooleanStateLabel.java
@@ -1,15 +1,17 @@
 package rocketview;
 
 import cansocket.*;
-import widgets.*;
+import javax.swing.*;
 
-public class BooleanStateLabel extends StateLabel implements CanObserver
+public class BooleanStateLabel extends JLabel implements CanObserver
 {
+	protected final String name;
 	protected final int id;
 
 	public BooleanStateLabel(String name, int id)
-	{
-		super(name);
+	{		
+		super(name + ": -");
+		this.name = name;
 		this.id = id;
 	}
 
@@ -18,8 +20,9 @@ public class BooleanStateLabel extends StateLabel implements CanObserver
 		if(msg.getId() != id)
 			return;
 
-		byte b = msg.getData8(0);
-		setKnown(b == 0 || b == 1);
-		setState(b != 0);
+		if (msg.getData8(0) == 0)
+			setText(name + ": Off");
+		else
+			setText(name + ": On");
 	}
 }
