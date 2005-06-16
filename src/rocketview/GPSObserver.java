3a7c1aed1b9c83c833bbc672b8172f46279f1c06
twelch <twelch@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-16 05:57:40 +0000

    added GPS solution quality, validity, locked checking
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2299 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/GPSObserver.java
+++ src/rocketview/GPSObserver.java
@@ -25,56 +25,60 @@ class GPSObserver extends JPanel implements CanObserver
 	protected static DecimalFormat minFmt = new DecimalFormat("00.000");
 	
 	// fields
-	protected int visible = 0;
 	protected int used = 0;
-	protected StringBuffer latString;
-	protected StringBuffer lonString;
+	protected int visible = 0;
+	protected int solution = 0;
+	protected int validity = 0;
+	
 
-	protected final JLabel stateLabel = new JLabel("GPS Node: (-)");
 	protected final JLabel latLabel = new JLabel("Lat: -");
 	protected final JLabel lonLabel = new JLabel("Lon: -");
 	protected final JLabel altLabel = new JLabel("Alt: -");
 	protected final TimeObserver time = new TimeObserver();
 	protected final JLabel satsLabel = new JLabel("Sats: -/-");
-	protected final LockStateLabel lockLabel = new LockStateLabel();
-	protected final JLabel solutionLabel = new JLabel("Solution: -");
-	protected final JLabel validityLabel = new JLabel("Validity: -");
+	//protected final LockStateLabel lockLabel = new LockStateLabel();
+	protected final JLabel lockLabel = new JLabel("Locked: -");
+	protected final JLabel solLabel = new JLabel("Solution: -");
+	protected final JLabel valLabel = new JLabel("Validity: -");
 
 	public GPSObserver(CanDispatch dispatch)
 	{
 		dispatch.add(this);
 		dispatch.add(time);
-		dispatch.add(lockLabel);
+		//dispatch.add(lockLabel);
 		setLayout(new GridBoxLayout());
-		add(stateLabel);
 		add(latLabel);
 		add(lonLabel);
 		add(altLabel);
 		add(time);
 		add(satsLabel);
 		add(lockLabel);
-		add(solutionLabel);
-		add(validityLabel);
+		add(solLabel);
+		add(valLabel);
 	}
 
 	public void message(CanMessage msg)
 	{
+		StringBuffer labelString = new StringBuffer();
+		StringBuffer solString = new StringBuffer();
+		StringBuffer valString = new StringBuffer();
+		
+		boolean solCheck = false;
+		boolean valCheck = false;
+		
 		switch(msg.getId())
 		{
-			case CanBusIDs.GPS_REPORT_MODE:
-				stateLabel.setText("GPS node: " + stateText(msg.getData8(0)));
-				return;
 			case CanBusIDs.FC_GPS_HEIGHT:
 				altLabel.setText("Alt: " + (msg.getData32(0) / (float)100.0) + 'm');
 				return;
 			case CanBusIDs.FC_GPS_LATLON:
-				latString = new StringBuffer();
-				dir(latString, msg.getData32(0), 'N', 'S');
-				latLabel.setText("Lat:  " + latString.toString());
+				labelString = new StringBuffer();
+				dir(labelString, msg.getData32(0), 'N', 'S');
+				latLabel.setText("Lat:  " + labelString.toString());
 				
-				lonString = new StringBuffer();
-				dir(lonString, msg.getData32(1), 'E', 'W');
-				lonLabel.setText("Lon: " + lonString.toString());
+				labelString = new StringBuffer();
+				dir(labelString, msg.getData32(1), 'E', 'W');
+				lonLabel.setText("Lon: " + labelString.toString());
 				return;
 			case CanBusIDs.FC_GPS_SATS_VIS:
 				visible = msg.getData8(0);
@@ -82,6 +86,74 @@ class GPSObserver extends JPanel implements CanObserver
 			case CanBusIDs.FC_GPS_SATS_USED:
 				used = msg.getData8(0);
 				break;
+			case CanBusIDs.FC_GPS_NAVSOL:
+				solution = msg.getData16(0);
+				validity = msg.getData16(1);
+				
+				// if bits 0,1,3 of byte 0 are 1 then there is a quality solution, else there's not so
+				// list the reasons why
+				if ((solution & 11) == 0)
+				{
+					solString.append("OK");
+					solCheck = true;
+				} else
+				{
+					if ((solution & 1) != 0)
+					{
+						labelString.append("Propogated, ");
+					} 
+					
+					if ((solution & 2) != 0)
+					{
+						labelString.append("Alt. Used, ");
+					} 
+					
+					if ((solution & 8) != 0)
+					{
+						labelString.append("PM");
+					}
+				}
+				solLabel.setText("Solution:  " + solString.toString());
+				
+				// if bits 0,2,3,4 of byte 3 are 1 then the solution is valid, else it's not so
+				// list the reasons why
+				if ((validity & 29) == 0)
+				{
+					valString.append("OK");
+					valCheck = true;
+				} else
+				{
+					if ((validity & 1) != 0)
+					{
+						valString.append("Alt. Used, ");
+					} 
+					
+					if((validity & 4) != 0) 
+					{
+						valString.append("<4 Sats, ");
+					} 
+					
+					if ((validity & 8) != 0)
+					{
+						valString.append("EHPE, ");
+					} 
+					
+					if ((validity & 16) != 0)
+					{
+						valString.append("EVPE");
+					}
+				}
+				valLabel.setText("Validity:  " + valString.toString());
+				
+				if (solCheck && valCheck)
+				{
+					lockLabel.setText("Locked: Yes");
+				} else
+				{
+					lockLabel.setText("Locked: No");
+				}
+				
+				return;
 			default:
 				return;
 		}
