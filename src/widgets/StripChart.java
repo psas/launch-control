a3d5b5ff5cf55db56160513f395bb27cc1c96107
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-06-18 20:50:35 +0000

    Quit using Lists, Iterators, etc.: just use a ring buffer. Still doesn't solve CPU problems.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2328 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/widgets/StripChart.java
+++ src/widgets/StripChart.java
@@ -9,18 +9,40 @@ public class StripChart extends JComponent
 {
 	protected static final Dimension MIN_CHART_DIM = new Dimension(300,50); 
 	
-	protected LinkedList ts = new LinkedList();
-	protected LinkedList ys = new LinkedList();
+	protected int oldest = 0;
+	protected int newest = 0;
+	protected float[] ts;
+	protected float[] ys;
+	protected int[] lx;
+	protected int[] ly;
 
+	protected int maxSampleRate = 2500;
 	protected float timeScale = 30; /* seconds */
 	protected float maxTimeScale = 60; /* seconds */
 	protected float minY = -20;
 	protected float maxY = 20;
 	
+	protected int roundup(int x)
+	{
+		int y = 1;
+		while(y < x)
+			y <<= 1;
+		return y;
+	}
 	
 	public Dimension getPreferredSize() 
 	{
 		return MIN_CHART_DIM;
+	}
+
+	public int getMaxSampleRate()
+	{
+		return maxSampleRate;
+	}
+
+	public void setMaxSampleRate(int maxSampleRate)
+	{
+		this.maxSampleRate = maxSampleRate;
 	}
 
 	public float getTimeScale()
@@ -51,70 +73,54 @@ public class StripChart extends JComponent
 
 	public void addPoint(float t, float y)
 	{
-		synchronized(ts)
+		if(ts == null)
 		{
-			ts.add(new Float(t));
-			ys.add(new Float(y));
+			ts = new float[roundup((int) Math.ceil(maxSampleRate * maxTimeScale))];
+			ys = new float[ts.length];
+
+			lx = new int[ts.length];
+			ly = new int[ys.length];
 		}
+		ts[newest & (ts.length - 1)] = t;
+		ys[newest & (ys.length - 1)] = y;
+		++newest;
 		repaint();
 	}
 
 	protected void paintComponent(Graphics g)
 	{
-		if (ts.size() == 0)
+		// Freeze "newest" at this instant
+		int current = newest;
+		if(current == oldest)
 			return;
 
-		int i;
-		int[] lx, ly;
+		if(current - oldest > ts.length)
+			oldest = current - ts.length;
 
-		synchronized(ts)
+		final Insets insets = getInsets();
+		final int width = getWidth() - insets.left - insets.right;
+		final int height = getHeight() - insets.top - insets.bottom;
+
+		// scale to the window
+		final float old = ts[(current - 1) & (ts.length - 1)] - timeScale;
+		final float sx =  width / timeScale;
+		final float sy = -height / (maxY - minY);
+		final float tx = -old * sx + insets.left + 0.5f;
+		final float ty = -maxY * sy + insets.top + 0.5f;
+
+		// start at the most recent point and draw backwards in time
+		int i = 0;
+		while(current != oldest)
 		{
-			final float now = ((Float) ts.get(ts.size() - 1)).floatValue();
-			float old;
-			ListIterator ti, yi;
-
-			// remove old points that will never be drawn again
-			old = now - getMaxTimeScale();
-			ti = ts.listIterator();
-			while(ti.hasNext())
-			{
-				float cur = ((Float) ti.next()).floatValue();
-				if(cur < now && cur > old)
-					break;
-			}
-
-			int lastold = ti.previousIndex();
-			ts.subList(0, lastold).clear();
-			ys.subList(0, lastold).clear();
-
-			// start at the most recent point and draw backwards in time
-			old = now - getTimeScale();
-			lx = new int[ts.size()];
-			ly = new int[ys.size()];
-
-			Insets insets = getInsets();
-			int width = getWidth() - insets.left - insets.right;
-			int height = getHeight() - insets.top - insets.bottom;
-
-			// scale to the window
-			float sx =  width / getTimeScale();
-			float sy = -height / (maxY - minY);
-			float tx = -old * sx + insets.left + 0.5f;
-			float ty = -maxY * sy + insets.top + 0.5f;
-
-			ti = ts.listIterator(ts.size());
-			yi = ys.listIterator(ys.size());
-
-			for(i = 0; ti.hasPrevious(); ++i)
-			{
-				float t = ((Float) ti.previous()).floatValue();
-				float y = ((Float) yi.previous()).floatValue();
-				if(t <= old)
-					break;
-				lx[i] = (int) (t * sx + tx);
-				ly[i] = (int) (y * sy + ty);
-			}
+			--current;
+			int ti = current & (ts.length - 1);
+			lx[i] = (int) (ts[ti] * sx + tx);
+			if(lx[i] < insets.left)
+				break;
+			ly[i] = (int) (ys[ti] * sy + ty);
+			++i;
 		}
+
 		g.drawPolyline(lx, ly, i);
 	}
 }
