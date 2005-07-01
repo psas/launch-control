eac37caaf8486bb816fd7e06bc6a59c6c41869ff
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-07-01 00:26:40 +0000

    Make battery label match spec; clean up code.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2473 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/APSObserver.java
+++ src/rocketview/APSObserver.java
@@ -5,14 +5,6 @@ import cansocket.*;
 import java.text.*;
 import javax.swing.*;
 
-/*----------------------------------------------------------------
- * -- not implemented --
- * Handles message id PowerID
- * Updates the APS box with status
- *
- *   display: APS bus: xx.xxV  batt: x.xxA xx.xxxAHr
- *          : Umb: [on/off] Power: [on/off] Charge: [on/off] 
- */
 class APSObserver extends JPanel implements CanObserver
 {
 	protected final DecimalFormat fmt = new DecimalFormat("0.000");
@@ -20,9 +12,9 @@ class APSObserver extends JPanel implements CanObserver
 	protected final JLabel umbLabel = new JLabel("Umbilical: -");
 	protected final JLabel busLabel = new JLabel();
 
-	protected double voltage = -1;
-	protected double current = -1;
-	protected double charge = -1;
+	protected String voltage = "-";
+	protected String current = "-";
+	protected String charge = "-";
 	
 	protected void add(CanDispatch dispatch, BooleanStateLabel label)
 	{
@@ -53,27 +45,10 @@ class APSObserver extends JPanel implements CanObserver
 		setText();
 	}
 
-    protected void setText()
-    {
-	StringBuffer b = new StringBuffer("APS bus: ");
-
-	if (voltage != -1) 
-		b.append(fmt.format(voltage)).append(" V, ");
-	else 
-		b.append("xx.xxV ");
-
-	if (current != -1)
-		b.append(fmt.format(current)).append(" A, ");
-	else 
-		b.append("x.xxA ");
-
-	if (charge != -1)
-		b.append(fmt.format(charge)).append(" Ah");
-	else 
-		b.append("batt: xx.xxxAHr");
-
-	busLabel.setText(b.toString());
-    }
+	protected void setText()
+	{
+		busLabel.setText("Battery: " + voltage + "V " + current + "A " + charge + "Ah");
+	}
 
     public void message(CanMessage msg)
     {
@@ -82,14 +57,14 @@ class APSObserver extends JPanel implements CanObserver
 	    case CanBusIDs.APS_DATA_VOLTS:
 		short counts = msg.getData16(0);
 		//counts &= 0xffff;   // unsigned.  What the heck?
-		voltage = counts * 0.033488;  // * 5 / 1024 / 0.14581;
+		voltage = fmt.format(counts * (5 / 1024 / 0.14581));
 		break;
 	    case CanBusIDs.APS_DATA_AMPS:
-		current = 768.05 / msg.getData32(0);
+		current = fmt.format(768.05 / msg.getData32(0));
 		break;
 	    case CanBusIDs.APS_DATA_CHARGE:
 			// 853.4 * 10^-6 Ah / count
-		charge = 853.4e-6 * msg.getData16(0);
+		charge = fmt.format(853.4e-6 * msg.getData16(0));
 		break;
 		case CanBusIDs.UMB_REPORT_CONNECTOR:
 			if (msg.getData8(0) == 0)
