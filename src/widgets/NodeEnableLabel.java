55b92b08931d10c95d04bebcb8f28ae5b85c56d3
iano <iano@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-07-14 05:45:44 +0000

    Try to fix a buffer overrun.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2548 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/widgets/NodeEnableLabel.java
+++ src/widgets/NodeEnableLabel.java
@@ -7,7 +7,7 @@ import java.lang.reflect.*;
 public class NodeEnableLabel extends NodeStateLabel
 {
 	protected final int id;
-	protected static final int length = 8192;
+	protected static final int length = 1<<13;	// 8192: must be a power of two
 	protected final int[] times = new int[length];
 
 	protected int next_out, next_in;
@@ -21,6 +21,11 @@ public class NodeEnableLabel extends NodeStateLabel
 		update(0);
 	}
 
+	private int wrap(int index)
+	{
+		return index & (length-1);		// (in % length)
+	}
+
 	public void message(CanMessage msg)
 	{
 		super.message(msg);
@@ -30,13 +35,13 @@ public class NodeEnableLabel extends NodeStateLabel
 		if(now == 0)
 			return;
 
-		// If time has gone backwards, flush and start over.
-		if(now < last_time)
-			next_out = next_in;
+		// If time has gone backwards or next_in overflowed, flush and start over.
+		if(now < last_time || next_in < 0)
+			next_out = next_in = 0;
 
 		// If this is *our* message, note another instance.
 		if(msg.getId() == id)
-			times[next_in++ & (length - 1)] = now;
+			times[wrap(next_in++)] = now;
 
 		// Repaint every quarter second.
 		if(now - last_time >= 25 || now < last_time)
@@ -48,14 +53,17 @@ public class NodeEnableLabel extends NodeStateLabel
 
 	private void update(int now)
 	{
+		// average msgs/time over at most the last three seconds
 		final int tenths;
-		while(next_out != next_in && times[next_out & (length - 1)] <= now - 300)
+		while(next_out != next_in && now - times[wrap(next_out)] >= 300)
 			++next_out;
 
-		if(next_in - next_out <= 1)
-			tenths = 0;
+		if (now > times[wrap(next_out)])
+		{
+			tenths = (int) (1000f * (next_in - next_out) / (now - times[wrap(next_out)]));
+			setDetail("" + (tenths / 10f) + "/sec");
+		}
 		else
-			tenths = (int) (1000f * (next_in - next_out) / (now - times[next_out & (length - 1)]));
-		setDetail("" + (tenths / 10f) + "/sec");
+			setDetail("0/sec");
 	}
 }
