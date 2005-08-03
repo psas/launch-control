ba6deb878147b5b60b6a24d6c2829d0b65365d1c
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-08-03 03:06:47 +0000

    Keep rates together; don't include single-state tests.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2606 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/IMUStateObserver.java
+++ src/rocketview/IMUStateObserver.java
@@ -6,31 +6,21 @@ import javax.swing.border.*;
 import widgets.StateGrid;
 import cansocket.*;
 
-public class IMUStateObserver extends JPanel implements CanObserver{
-
+public class IMUStateObserver extends JPanel
+{
 	public IMUStateObserver(CanDispatch dispatch)
 	{
 		setBorder(new TitledBorder("IMU"));
 		setLayout(new GridBoxLayout());
 
-		dispatch.add(this);
-				
 		add(StateGrid.getLabel("IMU"));
 		add(StateGrid.getLabel("IMU_ACCEL_DATA"));
-		add(StateGrid.getLabel("SANE_IMU_ACCEL"));
-		add(StateGrid.getLabel("BOOST_IMU"));
 		add(StateGrid.getLabel("IMU_GYRO_DATA"));
-		add(StateGrid.getLabel("DROGUE_IMU"));
-		add(StateGrid.getLabel("ARMING_IMU_FAST"));
 		add(StateGrid.getLabel("PRESS_REPORT_DATA"));
 		add(StateGrid.getLabel("TEMP_REPORT_DATA"));
+		add(StateGrid.getLabel("SANE_IMU_ACCEL"));
 		add(StateGrid.getLabel("PRESSURE_VALID"));
 		add(StateGrid.getLabel("QUIET_PRESSURE_BASE"));
+		add(StateGrid.getLabel("ARMING_IMU_FAST"));
 	}
-	
-	
-	public void message(CanMessage msg)
-   {
-		
-   }
 }
