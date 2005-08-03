207c33a1846a48857dfa01847e5b0c560c327ba3
jamey <jamey@6115d4fc-9e01-0410-8836-dad5f2c52705>
2005-08-03 03:51:36 +0000

    Don't implement CanObserver if you don't need to.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2611 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/ATVObserver.java
+++ src/rocketview/ATVObserver.java
@@ -6,27 +6,17 @@ import javax.swing.border.*;
 import widgets.*;
 import cansocket.*;
 
-public class ATVObserver extends JPanel implements CanObserver 
+public class ATVObserver extends JPanel
 {
-
 	public ATVObserver(CanDispatch dispatch)
 	{
 		setBorder(new TitledBorder("ATV"));
 		setLayout(new GridBoxLayout());
 
-		dispatch.add(this);
-		
 		add(StateGrid.getLabel("ATV"));
 		add(StateGrid.getLabel("ATV_POWER_CAMERA"));
 		add(StateGrid.getLabel("ATV_POWER_OVERLAY"));
 		add(StateGrid.getLabel("ATV_POWER_TX"));
 		add(StateGrid.getLabel("ATV_POWER_PA"));
-		
-		
 	}
-	
-	public void message(CanMessage msg)
-   {
-		
-   }
 }
