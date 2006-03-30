3cdb5e61dba628d45a99ef37644482d02d8a8024
josh <josh@6115d4fc-9e01-0410-8836-dad5f2c52705>
2006-03-30 05:57:34 +0000

    Add copyright notices and GPL license notices to all *.java files under the java hierarchy.  Names in copyright notices derived from svn log.
    git-svn-id: http://svn.psas.pdx.edu/svn/psas/trunk/java@2829 6115d4fc-9e01-0410-8836-dad5f2c52705

--- src/rocketview/APSObserver.java
+++ src/rocketview/APSObserver.java
@@ -1,3 +1,21 @@
+/* Copyright 2005 Ian Osgood, Jamey Sharp, Max Ehrenfreund, Peter Welte,
+ *                Tim Welch
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * Portland State Aerospace Society (PSAS) is a student branch chapter of the
+ * Institute of Electrical and Electronics Engineers Aerospace and Electronics
+ * Systems Society. You can reach PSAS at info@psas.pdx.edu.  See also
+ * http://psas.pdx.edu/
+ */
 package rocketview;
 
 import cansocket.*;
